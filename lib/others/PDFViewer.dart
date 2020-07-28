e1cecb4f48cee09e9fd0416f9aec03742a956181
Sanjog Satyarthi <sanjog12799@gmail.com>
2020-07-28 16:05:15 +0530

    28/07/2020

--- lib/others/PDFViewer.dart
+++ lib/others/PDFViewer.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:sms/sms.dart';
 import 'package:share_extend/share_extend.dart';
 
 class PdfProviderScreen extends StatelessWidget {
@@ -16,10 +15,19 @@ class PdfProviderScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 	  return Scaffold(
 		  appBar: AppBar(
-			  title: Text("Challan Generator"),
+			  iconTheme: IconThemeData(
+					  color: Colors.blue[100]
+			  ),
+			  title: Text("Challan Generator",style: TextStyle(
+				  fontFamily: "OpenSans",
+				  fontWeight: FontWeight.bold,
+				  letterSpacing: 1.0,
+				  color: Colors.blue[100],),),
+			  centerTitle: true,
+			  backgroundColor: Colors.blueGrey[700],
 			  actions: <Widget>[
 			  	FlatButton(
-					  child: Text("Share"),
+					  child: Icon(Icons.share,color: Colors.white,),
 					  onPressed: (){
 					  	shareChallan();
 					  },
@@ -27,6 +35,12 @@ class PdfProviderScreen extends StatelessWidget {
 			  ],
 		  ),
 	    body: Container(
+		    decoration: BoxDecoration(
+			    image: DecorationImage(
+					    image: AssetImage("images/12.jpg"),
+					    fit: BoxFit.cover
+			    ),
+		    ),
 		    padding: EdgeInsets.all(10),
 		    child: Stack(
 		      children: <Widget>[
