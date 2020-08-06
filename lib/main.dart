99f6566ad43cb474e6ed1d20cc77193fff71cb13
Sanjog Satyarthi <sanjog12799@gmail.com>
2020-08-06 20:25:49 +0530

    06/08/2020

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:laundry/Wrapper.dart';
 //import 'package:laundry/authentication/AuthScreens/Login.dart';
 //import 'package:laundry/pick_drop_ui/home_page.dart';
-import 'package:laundry/pick_drop_ui/pages/customer_end_work/customer_end.dart';
+//import 'package:laundry/pick_drop_ui/pages/customer_end_work/customer_end.dart';
 //import 'package:laundry/pick_drop_ui/pages/work_page_functionalities/during_navigation.dart';
 //import 'authentication/AuthScreens/Login.dart';
 //import 'pick_drop_ui/home_page.dart';
@@ -19,7 +19,7 @@ class Start extends StatelessWidget {
           canvasColor: Colors.transparent,
           scaffoldBackgroundColor: Colors.white
       ),
-      home: CustomerEnd(),
+      home: Wrapper(),
     );
   }
 }
